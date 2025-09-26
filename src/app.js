b2c56846b6b39365be7376f2641ee4ebe5788aa6
Vitor Mansque <mansquevitor@gmail.com>
2025-09-25 23:42:46 -0300

    feat: ajuste da estilização do projeto

--- src/app.js
+++ src/app.js
@@ -6,8 +6,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', (req, res) => res.render('index'));
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => res.render('index'));
 app.post('/start-process', startProcess);
 app.get('/pedidos', getProcessStatus);
 app.post('/reset', resetProcess);
